d5d34138ff738903c0a0524e8dc02ca660619a44
Carolina Avila <carolinavila@me.com>
2020-07-27 15:17:51 -0400

    Book Exercises

---
+++ Basic_Intro_Exercises/exercise_four.rb
@@ -0,0 +1,12 @@
+array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# Append 
+p array.push(11)
+
+# OR
+
+# arr << 11
+
+#prepend 
+
+p array.unshift(0)
---
+++ Basic_Intro_Exercises/exercise_two.rb
@@ -0,0 +1,14 @@
+# In relation to exercise one, print out values greater than 5. 
+
+array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+array.each do |num|
+  if num > 5
+    puts num
+  end
+end 
+
+
+#OR
+
+array.each { |num| puts num if num > 5 }
