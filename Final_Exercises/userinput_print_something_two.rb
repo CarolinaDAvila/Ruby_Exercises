e40c564d1a229b97298a7570174d388bb6771e10
Carolina Avila <carolinavila@me.com>
2020-08-06 18:48:05 -0400

    done

--- Final_Exercises/userinput_print_something_two.rb
+++ Final_Exercises/userinput_print_something_two.rb
@@ -9,18 +9,3 @@ puts 'something' if choice == 'y'
 
 
 
-# loop do
-#   puts ">> Do you want me to print something? (y/n)"
-#   answer = gets.chomp.downcase
-#   if answer == 'y'
-#     puts 'I have printed something for you'
-#     break
-#   elsif answer == 'n'
-#     break
-#   else 
-#     "Invalid input! Please enter y or n"
-#   end 
-# end 
-
-
-
