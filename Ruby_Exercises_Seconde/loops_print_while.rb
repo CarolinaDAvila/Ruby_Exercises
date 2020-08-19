1db9dfcbee98b1542d563980414d21a159b4e77d
Carolina Avila <carolinavila@me.com>
2020-08-19 13:56:48 -0400

    Seconde Loops

---
+++ Ruby_Exercises_Seconde/loops_print_while.rb
@@ -0,0 +1,25 @@
+numbers = []
+
+while numbers.size < 5
+  numbers << rand(100)
+end 
+
+puts numbers
+
+
+
+
+
+
+
+# numbers = []
+# count = 0
+
+# while numbers
+#   count = rand(100)
+#   numbers << count 
+#   count += 1
+#   break if numbers.size >= 5 
+#   end
+
+#   p numbers 
